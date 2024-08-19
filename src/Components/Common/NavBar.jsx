ab5467216bb90762d39bb5d7f4556bdab7e1c836
gero rios <antenuccifc87@gmail.com>
2024-08-18 23:29:29 -0300

    Cambios en el carousel de imagenes con efecttos

--- src/Components/Common/NavBar.jsx
+++ src/Components/Common/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <>
     <div className='' id='pene'>
-    <nav className="navbar navbar-expand-lg bg-dark rounded-3">
+    <nav className="navbar navbar-expand-lg bg-dark ">
       <div className="container-fluid">
      <a className="navbar-brand text-white" href="#">TuTurno</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -1,49 +1,49 @@
-import React from 'react'
+import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import "../../assets/Styles/HeroSection.css"
+import "../../assets/Styles/HeroSection.css";
+import img1 from "../../assets/Img/1.jpeg";
+import img2 from "../../assets/Img/2.jpeg";
+import img3 from "../../assets/Img/3.jpeg";
 
 const HeroSection = () => {
   return (
-    <>
-     <Carousel interval={1500}>
+    <Carousel interval={3000}>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/1200x400?text=Clinica+1"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Bienvenido a la Clínica</h3>
-          <p>Atención médica de calidad a tu alcance.</p>
-        </Carousel.Caption>
+        <div className="carousel-image-container">
+          <img className="d-block w-100" src={img1} alt="First slide" />
+          <div className="overlay"></div>
+          <Carousel.Caption className="centered-caption">
+            <h2>Bienvenido a la Clínica</h2>
+            <p>Atención médica de calidad a tu alcance.</p>
+          </Carousel.Caption>
+        </div>
       </Carousel.Item>
 
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/1200x400?text=Clinica+2"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Nuestros Servicios</h3>
-          <p>Especialidades médicas adaptadas a tus necesidades.</p>
-        </Carousel.Caption>
+        <div className="carousel-image-container">
+          <img className="d-block w-100" src={img2} alt="Second slide" />
+          <div className="overlay"></div>
+          <Carousel.Caption className="centered-caption">
+            <h2>Nuestros Servicios</h2>
+            <p>Especialidades médicas adaptadas a tus necesidades.</p>
+          </Carousel.Caption>
+        </div>
       </Carousel.Item>
 
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/1200x400?text=Clinica+3"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Solicita tu Turno Ahora</h3>
-          <p>Fácil y rápido, agenda tu cita en línea.</p>
-        </Carousel.Caption>
+        <div className="carousel-image-container">
+          <img className="d-block w-100" src={img3} alt="Third slide" />
+          <div className="overlay"></div>
+          <Carousel.Caption className="centered-caption">
+            <h2>Solicita tu Turno Ahora</h2>
+            <p>Fácil y rápido, agenda tu cita en línea.</p>
+          </Carousel.Caption>
+        </div>
       </Carousel.Item>
     </Carousel>
-    </>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
+
+
